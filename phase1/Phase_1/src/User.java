65359c49d695d3803193dedd0ed65b0146788375
Arjun Pandher <42449099+ArjunPandher@users.noreply.github.com>
2020-06-18 15:57:45 -0400

    Finished the abstract entity class User Added: - Constructor which takes a username and a password - username and password variables plus getters and setters - dateCreated variable plus getter

--- phase1/Phase_1/src/User.java
+++ phase1/Phase_1/src/User.java
@@ -1,2 +1,58 @@
+import java.time.LocalDate;
+
 public abstract class User {
+    private String username;
+    private String password;
+    private LocalDate dateCreated;
+
+    /**
+     * Default constructor of User's subclasses.
+     * @param username this User's username.
+     * @param password this User's password.
+     */
+    public User (String username, String password){
+        this.username = username;
+        this.password = password;
+        dateCreated = LocalDate.now();
+    }
+
+    /**
+     * Getter for username
+     * @return this User's username
+     */
+    public String getUsername (){
+        return username;
+    }
+
+    /**
+     * Getter for password
+     * @return this User's password
+     */
+    public String getPassword (){
+        return password;
+    }
+
+    /**
+     * Getter for dateCreated
+     * @return this user's dateCreated (which is a LocalDate object)
+     */
+    public LocalDate getDateCreated (){
+        return dateCreated;
+    }
+
+    /**
+     * Setter for username
+     * @param username this User's new username
+     */
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    /**
+     * Setter for password
+     * @param password this User's new password
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
