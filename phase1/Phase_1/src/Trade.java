2a8c10798fa18c3fb5cfabe06d7d77d48f0b4f63
jineshpatel <jinesh.patel@mail.utoronto.ca>
2020-06-19 18:15:03 -0400

    Finished abstract entity class Trade Added: - constructor which takes a initiator, a receiver, a tradeDate, a location, and isPermanent - initiator, receiver, tradeDate, location, isPermanent, isConfirmed, isCompleted, numberOfEdits and return Date variables with getter and setters - method to increase numberOfEdits by one for every edit that occurs

--- phase1/Phase_1/src/Trade.java
+++ phase1/Phase_1/src/Trade.java
@@ -1,2 +1,195 @@
-public class Trade {
+import java.time.LocalDate;
+
+public abstract class Trade {
+    private User initiator;
+    private User receiver;
+    private LocalDate tradeDate;
+    private String location;
+    private boolean isPermanent;
+    private boolean isConfirmed;
+    private boolean isCompleted;
+    private int numberOfEdits;
+    private LocalDate returnDate;
+
+    /**
+     * Default constructor of Trade
+     * @param initiator the user who started this trade.
+     * @param receiver the user who will receive the item in the trade.
+     * @param location the location of the Trade
+     * @param tradeDate the date the Trade will occur
+     * @param isPermanent whether the Trade is permanent or temporary
+     */
+    public Trade(User initiator, User receiver, String location, LocalDate tradeDate, boolean isPermanent){
+        this.initiator = initiator;
+        this.receiver = receiver;
+        this.location = location;
+        this.tradeDate = tradeDate;
+        this.isPermanent = isPermanent;
+        isConfirmed = false;
+        isCompleted = false;
+        numberOfEdits = 0;
+        returnDate = null;
+    }
+
+    /**
+     * Converts this Trade into String representation.
+     * @return this Trade's String representation.
+     */
+    @Override
+    public String toString() {
+        return "Trade{" +
+                "initiator='" + initiator.toString() + '\'' +
+                ", receiver='" + receiver.toString() + '\'' +
+                ", tradeDate=" + tradeDate.toString() + '\'' +
+                ", location=" + location + '\'' +
+                ", permanent=" + isPermanent + '\'' +
+                ", isConfirmed=" + isConfirmed + '\'' +
+                ", isCompleted=" + isCompleted + '\'' +
+                ", numberOfEdits=" + numberOfEdits + '\'' +
+                ", returnDate=" + returnDate.toString() + '\'' +
+                '}';
+    }
+
+    /**
+     * Increase numberOfEdits by one every time an edit occurs
+     */
+    public void increaseEdits(){
+        numberOfEdits += 1;
+    }
+
+    /**
+     * Getter for initiator
+     * @return the Trade's initiator
+     */
+    public User getInitiator() {
+        return initiator;
+    }
+
+    /**
+     * Getter for receiver
+     * @return the Trade's receiver
+     */
+    public User getReceiver() {
+        return receiver;
+    }
+
+    /**
+     * Getter for tradeDate
+     * @return the Trade's tradeDate
+     */
+    public LocalDate getTradeDate() {
+        return tradeDate;
+    }
+
+    /**
+     * Getter for location
+     * @return the Trade's location
+     */
+    public String getLocation() {
+        return location;
+    }
+
+    /**
+     * Getter for permanent
+     * @return whether the Trade is permanent or not
+     */
+    public boolean isPermanent() {
+        return isPermanent;
+    }
+
+    /**
+     * Getter for isConfirmed
+     * @return whether the Trade is confirmed or not
+     */
+    public boolean isConfirmed() {
+        return isConfirmed;
+    }
+
+    /**
+     * Getter for isCompleted
+     * @return whether the Trade is completed or not
+     */
+    public boolean isCompleted() {
+        return isCompleted;
+    }
+
+    /**
+     * Getter for numberOfEdits
+     * @return the number of edits for the Trade
+     */
+    public int getNumberOfEdits() {
+        return numberOfEdits;
+    }
+
+    /**
+     * Getter for returnDate
+     * @return the Trade's returnDate
+     */
+    public LocalDate getReturnDate() {
+        return returnDate;
+    }
+
+    /**
+     * Setter for initiator
+     * @param initiator the Trade's new initiator
+     */
+    public void setInitiator(User initiator) {
+        this.initiator = initiator;
+    }
+
+    /**
+     * Setter for receiver
+     * @param receiver the Trade's new receiver
+     */
+    public void setReceiver(User receiver) {
+        this.receiver = receiver;
+    }
+
+    /**
+     * Setter for TradeDate
+     * @param tradeDate the Trade's new tradeDate
+     */
+    public void setTradeDate(LocalDate tradeDate) {
+        this.tradeDate = tradeDate;
+    }
+
+    /**
+     * Setter for location
+     * @param location the Trade's new location
+     */
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    /**
+     * Setter for permanent
+     * @param permanent whether the Trade is permanent or temporary
+     */
+    public void setPermanent(boolean permanent) {
+        this.isPermanent = permanent;
+    }
+
+    /**
+     * Setter for isConfirmed
+     * @param confirmed whether the Trade is confirmed or not
+     */
+    public void setConfirmed(boolean confirmed) {
+        isConfirmed = confirmed;
+    }
+
+    /**
+     * Setter for isCompleted
+     * @param completed whether the Trade is completed or not
+     */
+    public void setCompleted(boolean completed) {
+        isCompleted = completed;
+    }
+
+    /**
+     * Setter for returnDate
+     * @param returnDate the Trade's new returnDate
+     */
+    public void setReturnDate(LocalDate returnDate) {
+        this.returnDate = returnDate;
+    }
 }
