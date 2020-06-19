c0925b7411b00072f028b59028640fa878b5c024
Jeffrey Huang <jeffreyzt.huang@mail.utoronto.ca>
2020-06-19 14:03:29 -0400

    Added the two constructors for the Admin class.

--- phase1/Phase_1/src/Admin.java
+++ phase1/Phase_1/src/Admin.java
@@ -1,2 +1,10 @@
 public class Admin extends User{
+    public Admin(String username, String password) {
+        super(username, password);
+    }
+
+    //Alternative constructor used for reading from files, off of the User reading file
+    public Admin(String username, String password, String date){
+        super(username, password, date);
+    }
 }
