ab57ffa43daeb75f0384a76ef6626ee0fdc5a0c7
carlwu <carl.wu@mail.utoronto.ca>
2020-06-20 02:38:34 -0400

    changed the needed variables in item to private added javadocs to the constructor, getters, and setters

--- phase1/Phase_1/src/Item.java
+++ phase1/Phase_1/src/Item.java
@@ -1,10 +1,17 @@
 public class Item {
-    int qualityRating;
-    String name, category;
-    Trader owner;
-    int id;
+    private int qualityRating;
+    private String name, category;
+    private Trader owner;
+    private int id;
     static int idCounter = 0;
 
+    /**
+     * The default constructor of the item class
+     * @param name the string the item is called
+     * @param category the string the item is called
+     * @param owner the trader that the item belongs to
+     * @param qualityRating the int the represents the quality/condition of the item
+     */
     public Item(String name, String category, Trader owner, int qualityRating){
         this.name = name;
         this.category = category;
@@ -14,21 +21,38 @@ public class Item {
         idCounter ++;
     }
 
+    /**
+     * Gets the item's name.
+     * @return the string the item is called
+     */
     public String getName(){
            return name;
         }
 
+    /**
+     * Sets the item's name.
+      * @param name the string the item is called
+     */
     public void setName(String name){
         this.name = name;
     }
 
+    /**
+     * Gets the item's category.
+     * @return the string the item is categorized as
+     */
     public String getCategory(){
         return category;
     }
 
+    /**
+     * Sets the item's category.
+     * @param category the string the item is categorized as
+     */
     public void setCategory(String category){
         this.category = category;
     }
+
 
     public int getQualityRating(){
         return qualityRating;
