22d3eb723230168d845f53e70dcc51d6f436a891
Junhee Jung <junhee.jung@mail.utoronto.ca>
2020-06-21 20:29:21 -0400

    - added some more comments
    Signed-off-by: Junhee Jung <junhee.jung@mail.utoronto.ca>

--- phase1/Phase_1/src/AdminActions.java
+++ phase1/Phase_1/src/AdminActions.java
@@ -18,7 +18,7 @@ public class AdminActions {
     }
 
     public boolean addAdmins(Admin[] listOfAdmins){
-        //will be removed
+        //this line will be removed
         return true;
     }
 
@@ -36,14 +36,34 @@ public class AdminActions {
         return true;
     }
 
-    //parameter is trader's list of items that needs to be approved
+    //parameter is trader's list of items that needs to be approved and the item that need to removed
     //admin can remove item in "items" (because of aliasing, the item will also
     // be removed in Trader's list of items)
-    public boolean approveItems(Item[] items){
-        //will be removed
+
+    // or instead of passing item that needs to be removed, we could pass in the index
+    // where the item is located?
+    public boolean removeItems(Item[] items, Item item){
+
+        //this line will be removed
         return true;
 
     }
+
+    public boolean approveItems(Item[] items, Item item){
+        //this line will be removed
+        return true;
+
+    }
+    //other idea
+    /*
+    public boolean removeItems(ArrayList<Item> items, int[] listOfIndex){
+        for (int index: listOfIndex){
+            items.remove(index);
+        }
+    }
+    same for approveItems method
+     */
+
 
     public void changeLimit(int newLimit){
 
