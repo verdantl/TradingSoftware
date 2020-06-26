0e4cfe49092c78c1ff0f5a20f4c24c7194fbb97c
Jeffrey Huang <jeffreyzt.huang@mail.utoronto.ca>
2020-06-26 19:36:35 -0400

    Added the Presenter classes and some instance variables to the AdminSystem class

---
+++ phase1/Phase_1/src/AdminPrompts.java
@@ -0,0 +1,2 @@
+public class AdminPrompts {
+}
--- phase1/Phase_1/src/AdminSystem.java
+++ phase1/Phase_1/src/AdminSystem.java
@@ -1,2 +1,15 @@
+import java.util.ArrayList;
+
 public class AdminSystem {
+    private AdminActions adminActions;
+    private Admin currentAdmin;
+    private AdminPrompts adminPrompts;
+
+    public AdminSystem(Admin admin, ArrayList<Admin> admins){
+        currentAdmin = admin;
+        adminActions = new AdminActions(admins);
+        //May need to change constructor
+        adminPrompts = new AdminPrompts();
+
+    }
 }
