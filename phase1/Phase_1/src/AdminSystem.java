fa7d8b21601cbfdb133ce9bcc874de60e213f793
Jeffrey Huang <jeffreyzt.huang@mail.utoronto.ca>
2020-06-26 20:07:43 -0400

    Added the loop (tentative) to the AdminSystem, and some comments to the TraderSystem (plz read)

--- phase1/Phase_1/src/AdminSystem.java
+++ phase1/Phase_1/src/AdminSystem.java
@@ -1,15 +1,49 @@
 import java.util.ArrayList;
 
-public class AdminSystem {
+public class AdminSystem implements Runnable{
+    private boolean running;
     private AdminActions adminActions;
     private Admin currentAdmin;
     private AdminPrompts adminPrompts;
 
+
+    /**
+     * Constructor for the AdminSystem
+     * @param admin The admin user that has logged into the system
+     * @param admins A list of all of the admins of the program
+     */
     public AdminSystem(Admin admin, ArrayList<Admin> admins){
         currentAdmin = admin;
         adminActions = new AdminActions(admins);
         //May need to change constructor
         adminPrompts = new AdminPrompts();
+        //may want to change the following
+
+
+    }
+
+
+    //Everything below here right now is part of the loop
+    //This method helps set up some stuff
+    private void init(){
+        running = true;
+    }
+
+
+    /**
+     * Loop for running the program
+     */
+    @Override
+    public void run() {
+        //this is the loop for the system
+        while (running){
+            //all the stuff here
+        }
+    }
+
+    //This might be better off in phase 2 as a listener, but here we're updating every iteration
+    //and calling other methods that will update the controller
+    private void update(){
 
     }
 }
--- phase1/Phase_1/src/TraderSystem.java
+++ phase1/Phase_1/src/TraderSystem.java
@@ -1,2 +1,4 @@
 public class TraderSystem {
+    //BY THE WAY BEFORE Y'ALL START take a look at my AdminSystem loop and
+    //let me know in the chat if the loop works or if you want to make changes - Jeffrey
 }
