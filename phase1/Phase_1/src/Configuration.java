5158763eb1526a8b336163b15dd5e7285e92b434
Amir Azimi <amir.azimi@mail.utoronto.ca>
2020-06-29 14:07:15 -0400

    Fixed the Configuration class to instantiate the different use case classes.

--- phase1/Phase_1/src/Configuration.java
+++ phase1/Phase_1/src/Configuration.java
@@ -4,10 +4,25 @@ public class Configuration {
     ArrayList<Trader> traders;
     ArrayList<Admin> admins;
 
+    TraderActions traderActions;
+    AdminActions adminActions;
+    ItemManager itemManager;
+    TradeManager tradeManager;
 
-    public Configuration(ArrayList<Trader> traders, ArrayList<Admin> admins){
+    ArrayList<User> users;
+
+
+    public Configuration(ArrayList<Trader> traders, ArrayList<Admin> admins, ArrayList<Admin> adminRequests){
         this.traders = traders;
         this.admins = admins;
+        this.users = new ArrayList<>();
+        users.addAll(traders);
+        users.addAll(admins);
+
+        traderActions = new TraderActions(this.traders);
+        adminActions = new AdminActions(this.admins, adminRequests);
+        itemManager = new ItemManager();
+        tradeManager = new TradeManager();
     }
 
     public ArrayList<Trader> getTraders() {
@@ -22,7 +37,51 @@ public class Configuration {
         return admins;
     }
 
+    public TraderActions getTraderActions() {
+        return traderActions;
+    }
+
+    public void setTraderActions(TraderActions traderActions) {
+        this.traderActions = traderActions;
+    }
+
+    public AdminActions getAdminActions() {
+        return adminActions;
+    }
+
+    public void setAdminActions(AdminActions adminActions) {
+        this.adminActions = adminActions;
+    }
+
+    public ItemManager getItemManager() {
+        return itemManager;
+    }
+
+    public void setItemManager(ItemManager itemManager) {
+        this.itemManager = itemManager;
+    }
+
+    public TradeManager getTradeManager() {
+        return tradeManager;
+    }
+
+    public void setTradeManager(TradeManager tradeManager) {
+        this.tradeManager = tradeManager;
+    }
+
+    public ArrayList<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(ArrayList<User> users) {
+        this.users = users;
+    }
+
     public void setAdmins(ArrayList<Admin> admins) {
         this.admins = admins;
+    }
+
+    public void addToUsers(User user){
+        this.users.add(user);
     }
 }
