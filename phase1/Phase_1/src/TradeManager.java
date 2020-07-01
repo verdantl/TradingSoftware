74449807c58b3946473f4c8be54c52f0e5ac16cd
jineshpatel <jinesh.patel@mail.utoronto.ca>
2020-07-01 13:21:22 -0400

    Updated TradeManager: - changed editTradeMeeting, agreeToMeeting, confirmTrade to return strings to display to user Updated TraderSystem: - Used displayString from TraderPrompts to display updates to the user

--- phase1/Phase_1/src/TradeManager.java
+++ phase1/Phase_1/src/TradeManager.java
@@ -196,14 +196,15 @@ public class TradeManager {
      * @param date the new date for meeting of processedTrade
      * @param location the new location for location of processed Trade
      */
-    public void editTradeMeeting(LocalDate date, String location){
+    public String editTradeMeeting(LocalDate date, String location){
         Trader receiver = processedTrade.getReceiver();
         if((processedTrade.getNumberOfEdit(currentUser) + processedTrade.getNumberOfEdit(receiver)) == 6
                 && (!processedTrade.getIsAgreed(currentUser) || !processedTrade.getIsAgreed(receiver))){
             processedTrade.setTradeStatus("Cancelled");
+            return "The trade is cancelled due to both users reaching a limit of editing meeting 3 times.";
         }
         else if(processedTrade.getNumberOfEdit(currentUser) == 3){
-            System.out.println("You have run out of edit times and you cannot edit");
+            return "You have run out of edit times and you cannot edit";
         }
         else{
             processedTrade.setTradeDate(date);
@@ -212,6 +213,7 @@ public class TradeManager {
             processedTrade.setIsAgreed(currentUser, false);
             processedTrade.setIsAgreed(receiver, false);
             processedTrade.setTradeStatus("In Progress");
+            return "New date and location has been set. Trade in Progress";
         }
     }
 
@@ -219,15 +221,17 @@ public class TradeManager {
      * Allow the current user to agree to the processedTrade's meeting
      * Confirm the meeting iff both users have agreed the trade
      */
-    public void agreeToMeeting(){
+    public String agreeToMeeting(){
         if(LocalDate.now().isAfter(processedTrade.getTradeDate())){
-            System.out.println("request refused: You have past the trade date");
+            return "Request refused: You have past the trade date";
         }else{
             processedTrade.setIsAgreed(currentUser, true);
 
             if(processedTrade.getIsAgreed(currentUser) && processedTrade.getIsAgreed(processedTrade.getReceiver())){
                 processedTrade.setTradeStatus("Agreed, Waiting to be confirmed");
+                return "Both users have agreed to the trade meeting. Waiting to be confirmed.";
             }
+            return "You have agreed to the trade meeting. Waiting for other user to agree.";
         }
     }
 
@@ -235,17 +239,17 @@ public class TradeManager {
      * Allow the current user to confirm the processedTrade has took place
      * Complete the processedTrade iff both users have confirmed trade
      */
-    public void confirmTrade(){
+    public String confirmTrade(){
         if(LocalDate.now().isBefore(processedTrade.getTradeDate())){
-            System.out.println("request refused: The trade should not take place before the meeting");
+            return "Request refused: The trade should not take place before the meeting";
         }else{
             processedTrade.setIsConfirmed(currentUser,true);
             if(processedTrade.getIsConfirmed(currentUser) && processedTrade.getIsConfirmed(processedTrade.getReceiver())){
                 processedTrade.setCompleted(true);
                 processedTrade.setTradeStatus("Completed");
+                return "Both user have confirmed the trade happened. Trade is completed.";
             }
+            return "You have confirmed the trade happened. Waiting for other user to confirm.";
         }
     }
-
-
 }
