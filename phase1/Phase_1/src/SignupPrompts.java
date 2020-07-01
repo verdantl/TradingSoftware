2c92b4ff3c3aa5662b78adc1942849300e1a4d32
Jeffrey Huang <jeffreyzt.huang@mail.utoronto.ca>
2020-07-01 16:56:18 -0400

    Implemented the SignupSystem and SignupPrompts and created to new csv files

--- phase1/Phase_1/src/SignupPrompts.java
+++ phase1/Phase_1/src/SignupPrompts.java
@@ -1,2 +1,31 @@
 public class SignupPrompts {
+
+    public void adminOrTrader(){
+        System.out.println("Enter [0] if you would like to signup for an admin request." +
+                " Enter [1] to sign up for an account. Enter [2] to go back to the login screen");
+    }
+    public void displayCreateUserName(){
+        System.out.println("Please enter the username you would like to create.");
+    }
+
+    public void displayUserAvailable(boolean value){
+        if (value){
+            System.out.println("Username is available.");
+        }
+        else{
+            System.out.println("Username is unavailable. Try another.");
+        }
+    }
+
+    public void commandNotRecognized(){
+        System.out.println("Command not recognized. Returning to Login.");
+    }
+
+    public void displayCreatePassword(){
+        System.out.println("Please enter the password you would like to use for this account.");
+    }
+
+    public void displayAccountSuccessful(){
+        System.out.println("Account successfully created!");
+    }
 }
