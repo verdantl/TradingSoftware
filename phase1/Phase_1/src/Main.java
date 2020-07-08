0ec6864229abb55c4b9bdd2dd791113c185face7
Amir Azimi <amir.azimi@mail.utoronto.ca>
2020-07-07 23:47:43 -0400

    Fixed mistaken path change in main.

--- phase1/Phase_1/src/Main.java
+++ phase1/Phase_1/src/Main.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        LoginSystem ls = new LoginSystem("phase1/Phase_1/src/test_dummy.csv");
+        LoginSystem ls = new LoginSystem("src/test_dummy.csv");
         ls.run();
     }
 }
