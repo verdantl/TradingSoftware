4a46738bf3cec673fa30d7421bfcf1f7e9e7a6b2
Jeffrey Huang <jeffreyzt.huang@mail.utoronto.ca>
2020-07-07 23:52:14 -0400

    Added documentation to the OneWayTrade and TwoWayTrade

--- phase1/Phase_1/src/OneWayTrade.java
+++ phase1/Phase_1/src/OneWayTrade.java
@@ -21,19 +21,19 @@ public class OneWayTrade extends Trade{
     }
 
     /**
-     * Constructor for oneWayTrade for when the item is read in.
-     * @param initiator The trade's initiator
-     * @param receiver The trade offer's receiver
-     * @param location The trade's location
-     * @param tradeDate The trade date
-     * @param isPermanent Whether the trade is permanent
+     * Alternative constructor for TwoWayTrade
+     * @param initiator The user who wants to trade their item with another user
+     * @param receiver The user that the initiator wants to trade with
+     * @param location The location of the trade
+     * @param tradeDate The date of the trade
+     * @param isPermanent Whether the trade is permanent or not
      * @param isCompleted Whether the trade is completed
-     * @param returnDate The trade's return date- set to 0000-01-01 if trade is permanent
-     * @param isConfirmed Whether the trade is confirmed by each user
-     * @param numberOfEdits The number of edits made by each user
-     * @param isAgreed Whether the trade is agreed to by each user
-     * @param tradeStatus The trade's status
-     * @param item The item of the trade
+     * @param returnDate The return date of the trade
+     * @param isConfirmed Whether the trade is confirmed
+     * @param numberOfEdits The number of edits made to this trade
+     * @param isAgreed Whether the traders have agreed to this trade
+     * @param tradeStatus the status of the trade
+     * @param item The item being traded
      */
     public OneWayTrade(Trader initiator, Trader receiver, String location, LocalDate tradeDate, boolean isPermanent, boolean isCompleted,
                  LocalDate returnDate, HashMap<String, Boolean> isConfirmed, HashMap<String, Integer> numberOfEdits,
@@ -79,6 +79,10 @@ public class OneWayTrade extends Trade{
      */
     public Item getItem(){ return item; }
 
+    /**
+     * A getter for the item in this trade
+     * @return an arraylist containing the item in this trade
+     */
     @Override
     public ArrayList<Item> getItems() {
         ArrayList<Item> items = new ArrayList<>();
