0842e6511ba9b6d0df578fee6853fdd4ad6161f0
Jeffrey Huang <jeffreyzt.huang@mail.utoronto.ca>
2020-07-17 18:35:22 -0400

    Created the Configuration file, still trying to fix LoginSystem but made major changes

---
+++ phase2/Phase2 (new)/src/Configuration.java
@@ -0,0 +1,52 @@
+import gateway.ConfigReader;
+import gateway.ConfigWriter;
+import items.ItemManager;
+import signupsys.SignupSystem;
+import trades.TradeManager;
+import users.AdminActions;
+import users.TraderActions;
+
+import java.io.IOException;
+
+public class Configuration {
+    private ConfigReader configReader;
+    private ConfigWriter configWriter;
+
+
+    private TraderActions traderActions;
+    private AdminActions adminActions;
+    private ItemManager itemManager;
+    private TradeManager tradeManager;
+
+
+    public Configuration(String path) throws IOException {
+        configReader = new ConfigReader(path);
+        configWriter = new ConfigWriter();
+
+        traderActions = configReader.getTraderActions();
+        adminActions = configReader.getAdminActions();
+        itemManager = configReader.getItemManager();
+        tradeManager = configReader.getTradeManager();
+
+    }
+
+    public TraderActions getTraderActions(){
+        return traderActions;
+    }
+
+    public AdminActions getAdminActions() {
+        return adminActions;
+    }
+
+    public ItemManager getItemManager() {
+        return itemManager;
+    }
+
+    public TradeManager getTradeManager() {
+        return tradeManager;
+    }
+
+    public void saveInfo(String path){
+//        configWriter.saveFile(path, traderActions, adminActions);
+    }
+}
