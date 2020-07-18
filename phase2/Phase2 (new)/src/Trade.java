b73bd1d0fd6809333d5e9a785034605006b2cf0e
Junhee Jung <junhee.jung@mail.utoronto.ca>
2020-07-17 21:14:29 -0400

    added new variables
    Signed-off-by: Junhee Jung <junhee.jung@mail.utoronto.ca>

--- phase2/Phase2 (new)/src/Trade.java
+++ phase2/Phase2 (new)/src/Trade.java
@@ -1,8 +1,28 @@
+import java.util.List;
+
 public class Trade {
     private String initiator;
     private String receiver;
+    private List<String> items; //list of item ids they are going to trade
+    private int id;
+    private static int counter = 0;
     private boolean isPermanent;
 
+    /**
+     *
+     * @param init Initiator's username
+     * @param rec Receiver's username
+     * @param items List of item ids they are going to trade
+     * @param isPermanent Determines whether the trade is permanent
+     */
+    public Trade (String init, String rec, List<String> items, boolean isPermanent){
+        initiator = init;
+        receiver = rec;
+        this.items = items;
+        this.isPermanent = isPermanent;
+        id = counter;
+        counter++;
+    }
 
     /**Getter for whether or not the trade is permanent
      * @return whether or not the trade is permanent
@@ -17,4 +37,61 @@ public class Trade {
     public void setPermanent(boolean permanent) {
         isPermanent = permanent;
     }
+
+    /**
+     * Getter for id
+     * @return Id of the trade
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     *
+     * @return List of item ids
+     */
+    public List<String> getItems() {
+        return items;
+    }
+
+    /**
+     * Getter for initiator
+     * @return Initiator's username
+     */
+    public String getInitiator() {
+        return initiator;
+    }
+
+    /**
+     * Getter for receiver
+     * @return Receiver's username
+     */
+    public String getReceiver() {
+        return receiver;
+    }
+
+    /**
+     * Setter for initiator
+     * @param initiator Initiator's username
+     */
+    public void setInitiator(String initiator) {
+        this.initiator = initiator;
+    }
+
+    /**
+     * Setter of items
+     * @param items List of item ids
+     */
+    public void setItems(List<String> items) {
+        this.items = items;
+    }
+
+    /**
+     * Setter for receiver
+     * @param receiver Receiver's username
+     */
+    public void setReceiver(String receiver) {
+        this.receiver = receiver;
+    }
+
 }
