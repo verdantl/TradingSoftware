e61ed4e6dbdf8a5d8aad1de97aeacf144db917c6
Junhee Jung <junhee.jung@mail.utoronto.ca>
2020-07-17 21:36:24 -0400

    added a method that returns a Trade object
    Signed-off-by: Junhee Jung <junhee.jung@mail.utoronto.ca>

--- phase2/Phase2 (new)/src/TradeManager.java
+++ phase2/Phase2 (new)/src/TradeManager.java
@@ -1,8 +1,43 @@
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class TradeManager {
-    private HashMap<String, ArrayList<Trade>> trades;
+    private HashMap<String, List<Trade>> trades;
 
+
+    /**
+     *
+     * @param username Trader's username
+     * @return List of trades that the Trader has. If the username does not exist, return null
+     */
+    public List<Trade> getTrades(String username){
+        if (!trades.containsKey(username)){
+            return null;
+        }
+        return trades.get(username);
+    }
+
+    /**
+     *
+     * @param username Trader's username
+     * @param tradeId Id of the trade that is being return
+     * @return Trade that the Trader has. If the username or the tradeId does not exist, return null
+     */
+    public Trade getTrade(String username, int tradeId){
+        if (!trades.containsKey(username)){
+            return null;
+        }
+        return getTradeWithTradeId(trades.get(username), tradeId);
+    }
+
+    private Trade getTradeWithTradeId(List<Trade> listOfTrades, int tradeId){
+        for (Trade t: listOfTrades){
+            if (t.getId() == tradeId){
+                return t;
+            }
+        }
+        return null;
+    }
 
 }
