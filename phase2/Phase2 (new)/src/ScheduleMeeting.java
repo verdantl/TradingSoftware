1513e294177f04daeb2012b488b5c941be5206ee
Junhee Jung <junhee.jung@mail.utoronto.ca>
2020-07-17 21:36:04 -0400

    added a method that returns a meeting object
    Signed-off-by: Junhee Jung <junhee.jung@mail.utoronto.ca>

--- phase2/Phase2 (new)/src/ScheduleMeeting.java
+++ phase2/Phase2 (new)/src/ScheduleMeeting.java
@@ -1,17 +1,37 @@
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+
 
 public class ScheduleMeeting {
-    private  HashMap<String, ArrayList<Meeting>> meetingInventory;
+    private  HashMap<String, List<Meeting>> meetingInventory;
 
     public ScheduleMeeting(HashMap<String, ArrayList<Meeting>> meetingInventory){
         this.meetingInventory = meetingInventory;
     }
 
-    public Meeting getMeeting(String user, int index){
-        return meetingInventory.get(user).get(index);
+    /**
+     *
+     * @param user Username of the Trader
+     * @param tradeId Id of the Trade
+     * @return Return Meeting iff the username and the tradeId is valid
+     */
+    public Meeting getMeeting(String user, int tradeId){
+        if (!meetingInventory.containsKey(user)){
+            return null;
+        }
+
+        return getMeetingWithTradeId(meetingInventory.get(user), tradeId);
     }
 
+    private Meeting getMeetingWithTradeId(List<Meeting> meetings, int tradeId){
+        for (Meeting m: meetings){
+            if (m.getTradeId() == tradeId){
+                return m;
+            }
+        }
+        return null;
+    }
 
 
 }
