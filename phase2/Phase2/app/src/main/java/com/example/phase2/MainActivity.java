b35f4609546beaf9472d97f24066d6abcb5860bd
Jeffrey Huang <jeffreyzt.huang@mail.utoronto.ca>
2020-07-26 16:39:25 -0400

    Implemented some part of the login button clicked method in MainActivity

--- phase2/Phase2/app/src/main/java/com/example/phase2/MainActivity.java
+++ phase2/Phase2/app/src/main/java/com/example/phase2/MainActivity.java
@@ -4,9 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import com.example.phase2.phase2.*;
 
 public class MainActivity extends AppCompatActivity {
+    private final AdminActions adminActions;
+    private final TraderManager traderManager;
 
+    private int nextSystem;
+    private String nextUser;
+
+    public MainActivity(TraderManager traderManager, AdminActions adminActions){
+        this.traderManager = traderManager;
+        this.adminActions = adminActions;
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,7 +25,21 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onLoginClicked(View view){
+        EditText userEditText = findViewById(R.id.editTextTextPersonName);
+        EditText passEditText = findViewById(R.id.editTextTextPassword);
+        String username = userEditText.getText().toString();
+        String password = passEditText.getText().toString();
+        if (traderManager.login(username, password)){
+            nextUser = username;
+            nextSystem = 2;
+        }
+        else if (adminActions.checkCredentials(username, password)){
+            nextUser = username;
+            nextSystem = 3;
+        }
+        else{
 
+        }
     }
     public void onSignupClicked(View view){
 
