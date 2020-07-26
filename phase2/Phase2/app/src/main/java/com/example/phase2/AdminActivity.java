8dfa115a0601b47b9d31d0fd5db1bd58be2587eb
Junhee Jung <junhee.jung@mail.utoronto.ca>
2020-07-26 18:14:53 -0400

    worked on main menu page for the admin
    Signed-off-by: Junhee Jung <junhee.jung@mail.utoronto.ca>

--- phase2/Phase2/app/src/main/java/com/example/phase2/AdminActivity.java
+++ phase2/Phase2/app/src/main/java/com/example/phase2/AdminActivity.java
@@ -3,11 +3,36 @@ package com.example.phase2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class AdminActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
+    }
+
+    public void addRemoveAdmin(View view){
+
+    }
+
+    public void manageAccounts(View view){
+
+    }
+
+    public void addRemoveItems(View view){
+
+    }
+
+    public void viewStatus(View view){
+
+    }
+
+    public void changeLimits(View view){
+
+    }
+
+    public void changePassword(View view){
+
     }
 }
