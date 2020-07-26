e3af4f05868aa0a46241cf5681fb5ae36958579a
Jeffrey Huang <jeffreyzt.huang@mail.utoronto.ca>
2020-07-26 16:24:49 -0400

    Added the onClick methods to the MainActivity

--- phase2/Phase2/app/src/main/java/com/example/phase2/MainActivity.java
+++ phase2/Phase2/app/src/main/java/com/example/phase2/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.phase2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onLoginClicked(View view){
+
+    }
+    public void onSignupClicked(View view){
+
     }
 }
