bf575f867682615686d4ad7722dcae50f8e8ada2
Jeffrey Huang <jeffreyzt.huang@mail.utoronto.ca>
2020-07-26 18:27:00 -0400

    Added the login error message to strings

--- phase2/Phase2/app/src/main/java/com/example/phase2/LoginActivity.java
+++ phase2/Phase2/app/src/main/java/com/example/phase2/LoginActivity.java
@@ -3,6 +3,7 @@ package com.example.phase2;
 import android.content.Intent;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,30 +14,29 @@ public class LoginActivity extends AppCompatActivity {
     private final AdminActions adminActions;
     private final TraderManager traderManager;
 
-    private int nextSystem;
-    private String nextUser;
-
     public LoginActivity(TraderManager traderManager, AdminActions adminActions){
         this.traderManager = traderManager;
         this.adminActions = adminActions;
     }
+
     public void onLoginClicked(View view){
         EditText userEditText = findViewById(R.id.editTextTextPersonName);
         EditText passEditText = findViewById(R.id.editTextTextPassword);
         String username = userEditText.getText().toString();
         String password = passEditText.getText().toString();
+        Intent intent;
         if (traderManager.login(username, password)){
-            nextUser = username;
-            nextSystem = 2;
-        }
-        else if (adminActions.checkCredentials(username, password)){
-            nextUser = username;
-            nextSystem = 3;
-        }
-        else{
 
         }
+        else if (adminActions.checkCredentials(username, password)){
+
+        }
+        else{
+            TextView textView = findViewById(R.id.loginerror);
+            textView.setText(R.string.login_error);
+        }
     }
+
     public void onSignupClicked(View view){
         Intent intent = new Intent(this, SignupActivity.class);
         startActivity(intent);
