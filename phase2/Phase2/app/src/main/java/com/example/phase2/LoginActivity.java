4f67789732ce3141e9d80cd11d3f3bf3d3b8204f
Jeffrey Huang <jeffreyzt.huang@mail.utoronto.ca>
2020-07-26 18:17:20 -0400

    Created a LoginActivity and a login xml file, and moved login functionality from MainActivity to LoginActivity

---
+++ phase2/Phase2/app/src/main/java/com/example/phase2/LoginActivity.java
@@ -0,0 +1,44 @@
+package com.example.phase2;
+
+import android.content.Intent;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.phase2.phase2.AdminActions;
+import com.example.phase2.phase2.TraderManager;
+
+public class LoginActivity extends AppCompatActivity {
+    private final AdminActions adminActions;
+    private final TraderManager traderManager;
+
+    private int nextSystem;
+    private String nextUser;
+
+    public LoginActivity(TraderManager traderManager, AdminActions adminActions){
+        this.traderManager = traderManager;
+        this.adminActions = adminActions;
+    }
+    public void onLoginClicked(View view){
+        EditText userEditText = findViewById(R.id.editTextTextPersonName);
+        EditText passEditText = findViewById(R.id.editTextTextPassword);
+        String username = userEditText.getText().toString();
+        String password = passEditText.getText().toString();
+        if (traderManager.login(username, password)){
+            nextUser = username;
+            nextSystem = 2;
+        }
+        else if (adminActions.checkCredentials(username, password)){
+            nextUser = username;
+            nextSystem = 3;
+        }
+        else{
+
+        }
+    }
+    public void onSignupClicked(View view){
+        Intent intent = new Intent(this, SignupActivity.class);
+        startActivity(intent);
+    }
+}
--- phase2/Phase2/app/src/main/java/com/example/phase2/MainActivity.java
+++ phase2/Phase2/app/src/main/java/com/example/phase2/MainActivity.java
@@ -3,45 +3,14 @@ package com.example.phase2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.EditText;
-import com.example.phase2.phase2.*;
 
 public class MainActivity extends AppCompatActivity {
-    private final AdminActions adminActions;
-    private final TraderManager traderManager;
 
-    private int nextSystem;
-    private String nextUser;
-
-    public MainActivity(TraderManager traderManager, AdminActions adminActions){
-        this.traderManager = traderManager;
-        this.adminActions = adminActions;
-    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 
-    public void onLoginClicked(View view){
-        EditText userEditText = findViewById(R.id.editTextTextPersonName);
-        EditText passEditText = findViewById(R.id.editTextTextPassword);
-        String username = userEditText.getText().toString();
-        String password = passEditText.getText().toString();
-        if (traderManager.login(username, password)){
-            nextUser = username;
-            nextSystem = 2;
-        }
-        else if (adminActions.checkCredentials(username, password)){
-            nextUser = username;
-            nextSystem = 3;
-        }
-        else{
 
-        }
-    }
-    public void onSignupClicked(View view){
-
-    }
 }
