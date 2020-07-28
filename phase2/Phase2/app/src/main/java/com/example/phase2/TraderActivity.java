940ddf0f0b58593ceed179118700c842982e1658
Jeffrey Huang <jeffreyzt.huang@mail.utoronto.ca>
2020-07-28 15:52:10 -0400

    Added the TraderActivity (empty)

--- phase2/Phase2/app/src/main/java/com/example/phase2/TraderActivity.java
+++ phase2/Phase2/app/src/main/java/com/example/phase2/TraderActivity.java
@@ -2,5 +2,13 @@ package com.example.phase2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Bundle;
+
 public class TraderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trader);
+    }
 }
