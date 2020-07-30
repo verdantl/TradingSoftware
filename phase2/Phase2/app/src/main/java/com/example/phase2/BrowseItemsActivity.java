997523f0bfd79024e4cd634c57c6c7bce38b427b
Arjun Pandher <42449099+ArjunPandher@users.noreply.github.com>
2020-07-30 19:06:53 -0400

    BrowseItemsActivity: - Started to set up class based off of AllTradersMenu TraderActivity: - Added currentTrader string. strings.xml: - Added a few relevant strings.
    Added files activity_browse_items.xml and fragment_location_choice.xml.

--- phase2/Phase2/app/src/main/java/com/example/phase2/BrowseItemsActivity.java
+++ phase2/Phase2/app/src/main/java/com/example/phase2/BrowseItemsActivity.java
@@ -1,4 +1,70 @@
 package com.example.phase2;
 
-public class BrowseItemsActivity {
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.phase2.phase2.ItemManager;
+import com.example.phase2.phase2.TraderManager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BrowseItemsActivity extends AppCompatActivity {
+
+    private ItemManager itemManager;
+    private String currentTrader;
+    private int chosenItem;
+    private boolean useLocation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Bundle bundle = getIntent().getExtras();
+        assert bundle != null;
+        itemManager = (ItemManager) bundle.getSerializable("ItemManager");
+        currentTrader = (String) bundle.getSerializable("CurrentTrader");
+        displayLocationChoice();
+        viewList();
+    }
+
+    public void viewList(){
+        final List<Integer> itemList = itemManager.getAllApprovedItemsIDs(currentTrader);
+
+        setContentView(R.layout.activity_browse_items);
+        ListView listView = findViewById(R.id.selectItem);
+        ArrayAdapter<Integer> allItemsAdapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, itemList);
+        listView.setAdapter(allItemsAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                displayFragment();
+                chosenItem = itemList.get(i);
+            }
+        });
+    }
+
+    public void onClickUseLocation(View view) {
+        useLocation = true;
+    }
+
+    public void onClickDontUseLocation(View view) {
+        useLocation = false;
+    }
+
+    public void displayFragment(){
+
+    }
+
+    public void displayLocationChoice(){
+
+    }
+
+    public void displayLocationChoiceFragment(){
+
+    }
 }
