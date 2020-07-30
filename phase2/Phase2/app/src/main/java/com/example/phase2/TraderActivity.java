1bf1c4715987a4907504abd83192a98313c090ef
Junhee Jung <junhee.jung@mail.utoronto.ca>
2020-07-30 10:57:45 -0400

    worked on creating Trader Menu
    Signed-off-by: Junhee Jung <junhee.jung@mail.utoronto.ca>

--- phase2/Phase2/app/src/main/java/com/example/phase2/TraderActivity.java
+++ phase2/Phase2/app/src/main/java/com/example/phase2/TraderActivity.java
@@ -2,7 +2,11 @@ package com.example.phase2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.phase2.phase2.TraderManager;
 
 public class TraderActivity extends AppCompatActivity {
 
@@ -10,5 +14,55 @@ public class TraderActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trader);
+    }
+/*
+    Methods here will be called when the corresponding button is clicked
+
+    You can pass around use case classes by calling Intent.putExtra(String key, UseCase class)
+    (Just like the dictionary)
+
+
+    You can get Use Case classes by calling getIntent().getSerializableExtra(String key)
+    (Don't forget to cast it)
+
+    You can pass around Strings, Integers, and other primitives through putExtra and get them through
+    getStringExtra()... etc.
+
+    If you reached the end of the activity (so no more activity to call), then insert finish();
+
+ */
+    public void browseAvailableItems(View view){
+        //TODO: Implement this method
+    }
+
+    public void browseOnGoingTrades(View view){
+        //TODO: Implement this method
+    }
+
+    public void editInventory(View view){
+        //TODO: Implement this method
+    }
+
+    public void editWishlist(View view){
+        //TODO: Implement this method
+    }
+
+    public void automaticTradeSuggestion(View view){
+        //TODO: Implement this method
+    }
+
+    public void viewUserInfo(View view){
+        //TODO: Implement this method
+    }
+
+    public void requestToUnfreeze(View view){
+        //TODO: Implement this method
+    }
+
+    public void changeTraderPassword(View view){
+        Intent i =  new Intent(this, ChangePasswordActivity.class);
+        i.putExtra("TraderManager", traderManager);
+        i.putExtra("CurrentTrader", currentTrader);
+        startActivity(i);
     }
 }
