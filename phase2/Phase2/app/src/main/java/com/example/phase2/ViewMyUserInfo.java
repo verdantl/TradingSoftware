14bcbd1ad67a3e5a93ce003034593ae1def417f2
Junhee Jung <junhee.jung@mail.utoronto.ca>
2020-08-03 15:27:56 -0400

    added new activity for View My User Info
    Signed-off-by: Junhee Jung <junhee.jung@mail.utoronto.ca>

---
+++ phase2/Phase2/app/src/main/java/com/example/phase2/ViewMyUserInfo.java
@@ -0,0 +1,14 @@
+package com.example.phase2;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ViewMyUserInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstance){
+        super.onCreate(savedInstance);
+        setContentView(R.layout.activity_view_my_user_info);
+    }
+}
