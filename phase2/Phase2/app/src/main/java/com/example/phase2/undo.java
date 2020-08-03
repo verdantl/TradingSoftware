d2fdb1fc2df683d64f724ec8db3e3bf98ca96986
WUQINYU <qinyu.wu@mail.utoronto.ca>
2020-08-03 19:21:26 -0400

    add undo enter interface

--- phase2/Phase2/app/src/main/java/com/example/phase2/undo.java
+++ phase2/Phase2/app/src/main/java/com/example/phase2/undo.java
@@ -2,13 +2,25 @@ package com.example.phase2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class undo extends AppCompatActivity {
+    private Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        intent = getIntent();
         setContentView(R.layout.activity_undo);
+    }
+
+    public void submit(View view){
+        EditText undoTraderText = findViewById(R.id.undoUser);
+        String chosenTrader = undoTraderText.getText().toString();
+        intent.putExtra("chosenTrader", chosenTrader);
+        startActivity(intent);
     }
 }
