a6ae4e9cda8601847fe7372e2b8a58acb3563676
Jeffrey Huang <jeffreyzt.huang@mail.utoronto.ca>
2020-08-03 23:58:27 -0400

    Edited MainActivity to now create the folders if they don't exist in the device, and read them in otherwise

--- phase2/Phase2/app/src/main/java/com/example/phase2/MainActivity.java
+++ phase2/Phase2/app/src/main/java/com/example/phase2/MainActivity.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import com.example.phase2.phase2.*;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.time.LocalDate;
@@ -21,38 +22,47 @@ public class MainActivity extends AppCompatActivity {
     private TraderManager traderManager;
     private TradeManager tradeManager;
 
-    //private final String ADMINPATH = "admins.ser";
-    //private final String ITEMPATH = "/phase2/configfiles/items.ser";
-    //private final String MEETINGPATH = "\\phase2\\configfiles\\meetings.ser";
-    //private final String TRADERPATH = "src/main/java/com/example/phase2/phase2/configfiles/traders.ser";
-    //private final String TRADEPATH = "src/main/java/com/example/phase2/phase2/configfiles/trade.ser";
+    private final String ADMINPATH = "admins.ser";
+    private final String ITEMPATH = "items.ser";
+    private final String MEETINGPATH = "meetings.ser";
+    private final String TRADERPATH = "traders.ser";
+    private final String TRADEPATH = "trade.ser";
 
-    private String nextUser;
-    private int nextSystem;
+    private final ConfigGateway configGateway = new ConfigGateway();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ConfigGateway configGateway = new ConfigGateway();
-//        try {
-//            adminActions = (AdminActions) configGateway.readInfo(ADMINPATH);
-//            itemManager = (ItemManager) configGateway.readInfo(ITEMPATH);
-//            meetingManager = (MeetingManager) configGateway.readInfo(MEETINGPATH);
-//            tradeManager = (TradeManager) configGateway.readInfo(TRADEPATH);
-//            traderManager = (TraderManager) configGateway.readInfo(TRADERPATH);
-//        } catch (IOException | ClassNotFoundException e) {
-//            e.printStackTrace();
-//        }
-        Context context = getApplicationContext();
-        //try {
-            //adminActions = (AdminActions) configGateway.readInfo(context.getFilesDir() + "admins.ser");
-            //meetingManager = (MeetingManager) configGateway.readInfo(context.getFilesDir() + "meetings.ser");
-            //itemManager = (ItemManager) configGateway.readInfo(context.getFilesDir() + "items.ser");
-            //tradeManager = (TradeManager) configGateway.readInfo(context.getFilesDir() + "trades.ser");
-            //traderManager = (TraderManager) configGateway.readInfo(context.getFilesDir() + "traders.ser");
+        startLogin();
+        loadClasses();
 
-        //} catch (IOException | ClassNotFoundException e) {
-            //e.printStackTrace();
-        //}
+    }
+
+    private void startLogin(){
+        Intent intent = new Intent(this, LoginActivity.class);
+        intent.putExtra("TraderManager", traderManager);
+        intent.putExtra("ItemManager", itemManager);
+        intent.putExtra("TradeManager", tradeManager);
+        intent.putExtra("MeetingManager", meetingManager);
+        intent.putExtra("AdminActions", adminActions);
+        startActivity(intent);
+    }
+
+    private void loadClasses(){
+        final File contextFilesDir = getApplicationContext().getFilesDir();
+        try {
+            adminActions = (AdminActions) configGateway.readInfo(contextFilesDir + "admins.ser");
+            meetingManager = (MeetingManager) configGateway.readInfo(contextFilesDir + "meetings.ser");
+            itemManager = (ItemManager) configGateway.readInfo(contextFilesDir + "items.ser");
+            tradeManager = (TradeManager) configGateway.readInfo(contextFilesDir + "trades.ser");
+            traderManager = (TraderManager) configGateway.readInfo(contextFilesDir + "traders.ser");
+
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+            downloadInitial();
+        }
+    }
+
+    private void downloadInitial(){
         HashMap<String, Admin> admins = new HashMap<>();
         admins.put("Admin", new Admin("Admin", "Wordpass", "2020-07-27", true));
         adminActions = new AdminActions(admins);
@@ -68,7 +78,6 @@ public class MainActivity extends AppCompatActivity {
         traderManager = new TraderManager(new HashMap<String, Trader>(), 3, 1, 0);
         traderManager.addTrader(new Trader("Trader1", "Password"));
         traderManager.addTrader(new Trader("Trader2", "Password2"));
-
         Trader traderFlagged = new Trader("Arjun", "Password3");
         traderFlagged.setFlagged(true);
         traderManager.addTrader(traderFlagged);
@@ -80,12 +89,10 @@ public class MainActivity extends AppCompatActivity {
         LocalDate tempDate = LocalDate.now();
         traderManager.addNewTrade("Arjun", tradeId,tempDate);
         traderManager.addNewTrade("Trader1", tradeId, tempDate);
-
         //CREATES THE MEETING
         meetingManager.createMeeting(tradeId, "Arjun", "Trader1", true);
         meetingManager.setMeetingInfo(tradeId, LocalDate.now(), LocalDate.now(),
                 "Toronto", "Toronto");
-
         adminActions.newAdmin("Admin2", "Wordpass");
         adminActions.newAdmin("Sup", "nothing");
         Trader traderUnfreeze = new Trader("Jeffrey", "Password4");
@@ -93,29 +100,13 @@ public class MainActivity extends AppCompatActivity {
         traderUnfreeze.setRequestToUnfreeze(true);
         traderManager.addTrader(traderUnfreeze);
         try {
-            configGateway.saveInfo(context.getFilesDir() + "admins.ser", adminActions);
-            configGateway.saveInfo(context.getFilesDir() + "meetings.ser", meetingManager);
-            configGateway.saveInfo(context.getFilesDir() + "traders.ser", traderManager);
-            configGateway.saveInfo(context.getFilesDir() + "trades.ser", tradeManager);
-            configGateway.saveInfo(context.getFilesDir() + "items.ser", itemManager);
+            configGateway.saveInfo(ADMINPATH, adminActions);
+            configGateway.saveInfo(MEETINGPATH, meetingManager);
+            configGateway.saveInfo(TRADERPATH, traderManager);
+            configGateway.saveInfo(TRADEPATH, tradeManager);
+            configGateway.saveInfo(ITEMPATH, itemManager);
         } catch (IOException e) {
             e.printStackTrace();
         }
-        Bundle bundle = getIntent().getExtras();
-        if (bundle != null) {
-            nextUser = bundle.getString("NextUser");
-            nextSystem = bundle.getInt("NextSystem");
-        }
-        else{
-            nextSystem = 0;
-        }
-
-        Intent intent = new Intent(this, LoginActivity.class);
-        intent.putExtra("TraderManager", traderManager);
-        intent.putExtra("ItemManager", itemManager);
-        intent.putExtra("TradeManager", tradeManager);
-        intent.putExtra("MeetingManager", meetingManager);
-        intent.putExtra("AdminActions", adminActions);
-        startActivity(intent);
     }
 }
