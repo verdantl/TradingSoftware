f91a135a1999eae8923aaa116e460a3f81824695
Jeffrey Huang <jeffreyzt.huang@mail.utoronto.ca>
2020-08-10 15:33:22 -0400

    Pushed javadoc for UpdatableBundleActivity

--- phase2/Phase2/app/src/main/java/com/example/phase2/UpdatableBundleActivity.java
+++ phase2/Phase2/app/src/main/java/com/example/phase2/UpdatableBundleActivity.java
@@ -6,11 +6,24 @@ import androidx.annotation.Nullable;
 
 public abstract class UpdatableBundleActivity extends BundleActivity {
 
+    /**
+     * Called when an activity you launched exits, giving you the requestCode you started it with,
+     * the resultCode it returned, and any additional data from it.
+     * @param requestCode The integer request code originally supplied to startActivityForResult(),
+     * allowing you to identify who this result came from.
+     * @param resultCode The integer result code returned by the child activity through its
+     * setResult().
+     * @param data An Intent, which can return result data to the caller (various data can be
+     * attached to Intent "extras").
+     */
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         updateUseCases();
     }
 
+    /**
+     * Updates the Manager classes in the bundle
+     */
     protected abstract void updateUseCases();
 }
