b2050fe22a7720be3d1fe1bb5dff5a223b775dad
WUQINYU <qinyu.wu@mail.utoronto.ca>
2020-08-10 14:11:15 -0400

    add disabled button

--- phase2/Phase2/app/src/main/java/com/example/phase2/TradeType.java
+++ phase2/Phase2/app/src/main/java/com/example/phase2/TradeType.java
@@ -1,10 +1,13 @@
 package com.example.phase2;
 
+import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -22,6 +25,7 @@ public class TradeType  extends AppCompatDialogFragment {
     final String[] status = {"Permanent", "Temporary"};
     private Boolean chosenWay;
     private Boolean chosenStatus;
+    private AlertDialog nestedDialog;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -29,6 +33,7 @@ public class TradeType  extends AppCompatDialogFragment {
         bundle = new Bundle();
         chosenStatus = false;
         chosenWay = false;
+        nestedDialog = (AlertDialog) nestedDialog();
     }
 
     @Override
@@ -44,33 +49,41 @@ public class TradeType  extends AppCompatDialogFragment {
 
     @NonNull
     @Override
-    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+    public Dialog onCreateDialog(@Nullable final Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         builder.setTitle("TradeType")
-                .setSingleChoiceItems(ways, 0, new DialogInterface.OnClickListener() {
+                .setSingleChoiceItems(ways, -1, new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
                         chosenWay = i == 0;
+                        AlertDialog dialog = (AlertDialog) getDialog();
+                        assert dialog != null;
+                        dialog.getButton(AlertDialog.BUTTON_POSITIVE).setEnabled(true);
                     }
                 })
                 .setPositiveButton("DONE", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
                         bundle.putBoolean("way", chosenWay);
-                        nestedDialog().show();
+                        dismiss();
+                        nestedDialog.show();
+                        nestedDialog.getButton(AlertDialog.BUTTON_POSITIVE).setEnabled(false);
                     }
                 });
 
-        return builder.create();
+            return builder.create();
+
+
     }
 
     public Dialog nestedDialog(){
-        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        final AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         builder.setTitle("TradeType")
-                .setSingleChoiceItems(status, 0, new DialogInterface.OnClickListener() {
+                .setSingleChoiceItems(status, -1, new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
                         chosenStatus = i == 1;
+                        nestedDialog.getButton(AlertDialog.BUTTON_POSITIVE).setEnabled(true);
                     }
                 })
                 .setPositiveButton("DONE", new DialogInterface.OnClickListener() {
@@ -90,6 +103,15 @@ public class TradeType  extends AppCompatDialogFragment {
 
         return builder.create();
 
+    }
+
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        AlertDialog dialog = (AlertDialog) getDialog();
+        assert dialog != null;
+        dialog.getButton(AlertDialog.BUTTON_POSITIVE).setEnabled(false);
     }
 
 
